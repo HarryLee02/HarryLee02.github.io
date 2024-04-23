7140d7b5dfa01e049805e3eacf578aeb7deb6a9b
harrylee02 <ngochieu0211.study@gmail.com>
2024-04-23 13:56:21 +0700

    Change index & fav

--- experience/script.js
+++ experience/script.js
@@ -65,11 +65,11 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "HarryLee's Portfolio";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
         document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href","/assets/images/favhand.png");
+        $("#favicon").attr("href","/assets/images/favicon.ico");
     }
 });
