550ad0db2148fda879a26b7357505fb6302fa4a6
harrylee <22520435@gm.uit.edu.vn>
2024-10-06 15:11:28 +0700

    Adjusting skills and delete bunch of pics

--- projects/script.js
+++ projects/script.js
@@ -39,6 +39,7 @@ function getProjects() {
         });
 }
 
+// <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
 
 function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
@@ -55,7 +56,6 @@ function showProjects(projects) {
         <div class="desc">
           <p>${project.desc}</p>
           <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
             <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
           </div>
         </div>
